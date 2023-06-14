406e896500586ef1130d1fd12d1a1ec0088b2756
Jonathan Vetter <Virtual.Java@mail.de>
2023-06-14 21:28:19 +0200

    Fix issue deque mutated during iteration see: https://github.com/MarlinFirmware/Marlin/issues/25813

--- buildroot/share/PlatformIO/scripts/marlin.py
+++ buildroot/share/PlatformIO/scripts/marlin.py
@@ -20,6 +20,8 @@ def copytree(src, dst, symlinks=False, ignore=None):
 
 def replace_define(field, value):
 	for define in env['CPPDEFINES']:
+		envdefs = env['CPPDEFINES'].copy()
+	for define in envdefs:
 		if define[0] == field:
 			env['CPPDEFINES'].remove(define)
 	env['CPPDEFINES'].append((field, value))
