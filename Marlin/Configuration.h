3440562d84ff74e5fb84efea95f5c4607584ae05
Jonathan Vetter <Virtual.Java@mail.de>
2023-06-15 18:53:01 +0200

    Add myName to modification for Vyper with glassbed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -91,7 +91,7 @@
 // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Paul Matthews, Vyper with TMC motherboard)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Jonathan Vetter, Vyper with TMC motherboard)" // Who made the changes.
 #define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -171,7 +171,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Anycubic Vyper"
+#define CUSTOM_MACHINE_NAME "Anycubic Vyper Glassbed"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
